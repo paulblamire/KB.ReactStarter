ec47c495c569ca45673da7710d55048cf7a188fe
Paul Blamire <paul_blamire@yahoo.com>
2016-03-12 21:26:34 +0000

    Ignore jspm_modules and delete jspm modules
    reinstall using npm install followed by jspm install instead

--- ReactStarter/jspm_packages/npm/react-router@2.0.1/modules/routerWarning.js
+++
@@ -1,8 +0,0 @@
-/* */ 
-"format cjs";
-import warning from 'warning'
-
-export default function routerWarning(falseToWarn, message, ...args) {
-  message = `[react-router] ${message}`
-  warning(falseToWarn, message, ...args)
-}
